c294f013163706812eb666fc4e83b4d1cecaf8c5
subharamv <subharam.v@clovetech.com>
2026-01-05 14:00:13 +0530

    Delete Downloads/cloveid-main/cloveid-main directory

--- Downloads/cloveid-main/cloveid-main/src/migrations/002_alter_vendor_sends.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE public.vendor_sends
-ADD COLUMN front_image_url TEXT,
-ADD COLUMN back_image_url TEXT,
-ADD COLUMN card_details JSONB;
--- Downloads/cloveid-main/cloveid-main/src/migrations/010_add_vendor_role.sql
+++
@@ -1,2 +0,0 @@
--- 1. Add 'vendor' to user_role_enum
-ALTER TYPE public.user_role_enum ADD VALUE IF NOT EXISTS 'vendor';
